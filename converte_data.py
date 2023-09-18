061377da22d1e1bd9e4f552c28002a89f6d3f64d
Sillas Rocha <125564261+scrocha@users.noreply.github.com>
2023-09-18 08:43:27 -0300

    adicionei docstring e doctest
    agora o módulo de converter datas possui docstring e doctestes simples.

--- converte_data.py
+++ converte_data.py
@@ -6,18 +6,45 @@ def receber_datas(datas: str):
     Recebe duas strings de datas separadas por um - e as devolve em uma lista
     separadamente.
 
-    Args:
-        datas: str
-          String com duas datas a serem separadas.
+    Parameters
+    ----------
+    datas: str
+        String com duas datas a serem separadas.
 
-    Returns:
-        list: Lista com as duas datas separadas.
+    Returns
+    -------
+    sep_datas: list
+        Lista com as duas datas separadas.
+
+    Teste simples que já deve ser geral para qualquer caso
+    >>> receber_datas("29 de Janeiro de 2005 - 25 de Março de 2003")
+    ['29 de Janeiro de 2005', '25 de Março de 2003']
     """
     sep_datas = datas.split(" - ")
     return sep_datas
 
 
 def converter_data(data: str):
+    """
+    Recebe uma string de data da forma "dia de mes(nome do mes) de ANO" e retorna no formato de
+    "ANO-mes-dia".
+
+    Parameters
+    ----------
+    data: str
+        String da data a ser convertida.
+    
+    Returns
+    -------
+    new_format: str
+        A string com a data do formato requerido
+
+    Exemplo simples da função para testar, como o tratamento foi feito no
+    input do usuário não deve ser necessário fazer tratamento extra.
+    
+    >>> converter_data("29 de Janeiro de 2005")
+    '2005-01-29'
+    """
     meses = {
         "Janeiro": "01",
         "Fevereiro": "02",
@@ -45,4 +72,7 @@ def converter_data(data: str):
 
     return new_format
 
+
+if __name__ == "__main__":
+    doctest.testmod(verbose=True)
 
