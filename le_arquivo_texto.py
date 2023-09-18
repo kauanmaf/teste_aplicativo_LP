d417f2fb211ddb6ebfeb224ebd9eef9a69b6e27f
GIOVANNA GROSSI MONTEIRO <B51076@fgv.edu.br>
2023-09-18 09:01:21 -0300

    Documentação do módulo

--- le_arquivo_texto.py
+++ le_arquivo_texto.py
@@ -1,3 +1,10 @@
+""" Documentação de módulo 
+
+Este módulo define a função ler_texto, que transforma o arquivo .txt 
+enviado pelo usuário em uma string a ser utilizada no cálculo em outros módulos.
+
+"""
+
 
 def ler_texto(nome_arquivo):
     with open(nome_arquivo, "r") as arquivo:
