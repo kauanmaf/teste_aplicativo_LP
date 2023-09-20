7736201ea462eca7cc1959decf9b319bb425bf84
Sillas Rocha <125564261+scrocha@users.noreply.github.com>
2023-09-20 07:58:36 -0300

    adicionei documentação a biblioteca de ferramentas
    documentação bem simples

--- ferramentas/__init__.py
+++ ferramentas/__init__.py
@@ -1,3 +1,7 @@
+""" Biblioteca com os módulos de ler arquivos, fazer manipulações com datas de strings
+e calcular as datas, ferramentas necessárias para o uso das funções.
+"""
+
 from .calculo_data import *
 from .converte_data import *
 from .le_arquivo_texto import *
