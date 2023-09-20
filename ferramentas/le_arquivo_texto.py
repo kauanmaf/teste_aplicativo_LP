11154aec745d4d9b61a31c549db35695e2f6aea9
Sillas Rocha <125564261+scrocha@users.noreply.github.com>
2023-09-20 07:51:06 -0300

    adicionei docstring ao leitor de texto
    adicionei também um arquivo com mais de uma data para testes.

--- ferramentas/le_arquivo_texto.py
+++ ferramentas/le_arquivo_texto.py
@@ -6,14 +6,18 @@ enviado pelo usuário em uma string a ser utilizada no cálculo em outros módul
 """
 
 
-def ler_texto(nome_arquivo):
-    """Função que lê datas de um arquivo de texto
+def ler_texto(nome_arquivo: str):
+    """Função que lê datas de um arquivo de texto, as datas devem estar como:
+    "dd" de "MM"(Nome do Mês) de "YYYY" - "dd" de "MM"(Nome do Mês) de "YYYY".
+    Sendo que se houver mais de um par de datas, deve estar em outra linha.
 
     Args:
-        nome_arquivo (_type_): _description_
+        nome_arquivo: str
+            Nome do arquivo .txt com as datas selecionadas.
 
     Returns:
-        list: Uma lista com cada
+        datas: list
+            Uma lista com cada um dos pares de datas lidos em cada linha.
     """
     with open(nome_arquivo, "r") as arquivo:
         datas = arquivo.read().splitlines()
